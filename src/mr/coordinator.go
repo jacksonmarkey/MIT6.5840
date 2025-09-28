8d80b3252d3dac1310541a8b5ef6b62bb1001553
fkacheese106 <jackson.markey@gmail.com>
2025-09-27 20:23:00 -0400

    adds TaskRequest RPC call with fake data between worker and coordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,42 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	files   []string
+	nReduce int
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) TaskRequest(args *TaskRequestArgs, reply *TaskRequestReply) error {
+	fmt.Println("Coordinator: TaskRequest recieved successfully!")
+	reply.IsMapTask = true
+	reply.NReduce = c.nReduce
+	// TODO: For each TaskRequest, pick a file from c.files, associate it with this worker
+	// in a concurrency-safe map, along with it's start time?
+	reply.FileName = c.files[0]
+	// Gonna need to use channels here to communicate between workers and coordinator
+	// Should it be owned/instantiated by the Coordinator or the Worker? hmm...
+	return nil
+}
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +50,26 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		files,
+		nReduce,
+	}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
