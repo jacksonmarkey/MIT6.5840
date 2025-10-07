e8f43e62978ee6127be00f016c623a45250be8c2
Jackson Markey <jackson.markey@gmail.com>
2025-10-07 10:55:56 -0400

    implements a lock using kv store on unreliable network

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,8 +1,6 @@
 package lock
 
 import (
-	"time"
-
 	"6.5840/kvsrv1/rpc"
 	kvtest "6.5840/kvtest1"
 )
@@ -34,30 +32,41 @@ func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 		clientID: kvtest.RandValue(8),
 	}
 	// You may add code here
-	_, _, err := ck.Get(lk.lockID)
-	if err == rpc.ErrNoKey {
-		ck.Put(lk.lockID, UnlockedState, 0)
-	}
+	// Initially I initialized the lock here, but then I changed Acquire
+	// to handle the case where we first acquire the lock instead
+	// _, _, err := ck.Get(lk.lockID)
+	// if err == rpc.ErrNoKey {
+	// 	for {
+	// 		err = ck.Put(lk.lockID, UnlockedState, 0)
+	// 		if err == rpc.OK || err == rpc.ErrVersion {
+	// 			break
+	// 		}
+	// 		// Keep looping until we get
+	// 	}
+	// }
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
 	for {
-		value, version, err := lk.ck.Get(lk.lockID)
-		if err != rpc.OK {
-			continue
+		value, version, getErr := lk.ck.Get(lk.lockID)
+		var putErr rpc.Err
+		switch getErr {
+		case rpc.ErrNoKey:
+			putErr = lk.ck.Put(lk.lockID, lk.clientID, 0)
+		case rpc.OK:
+			if value == lk.clientID {
+				// We already have the lock
+				return
+			}
+			if value == UnlockedState {
+				putErr = lk.ck.Put(lk.lockID, lk.clientID, version)
+			}
 		}
-		if value != UnlockedState {
-			// Special error if it's already locked to this specific client?
-			time.Sleep(time.Second)
-			continue
+		if putErr == rpc.OK {
+			return
 		}
-		err = lk.ck.Put(lk.lockID, lk.clientID, version)
-		if err != rpc.OK {
-			continue
-		}
-		return
 	}
 
 }
@@ -65,18 +74,29 @@ func (lk *Lock) Acquire() {
 func (lk *Lock) Release() {
 	// Your code here
 	for {
-		value, version, err := lk.ck.Get(lk.lockID)
-		if err != rpc.OK {
-			continue
+		value, version, getErr := lk.ck.Get(lk.lockID)
+		switch getErr {
+		case rpc.ErrNoKey:
+			// Lock has never been locked
+			return
+		case rpc.OK:
+			if value != lk.clientID {
+				// If locked to another client already, assume the following:
+				// 1. We successfully unlocked previously in the loop
+				// 2. The network lost our confirmation of that successful unlock
+				// 3. By the time we sent our current Get request, someone else
+				// 		acquired the lock
+				// In this case, we should return
+				return
+			}
+			// We do currently have the lock, so we need to release it
+			putErr := lk.ck.Put(lk.lockID, UnlockedState, version)
+			if putErr == rpc.OK {
+				return
+			}
+			// If we don't get an OK, it doesn't matter what specific error we got.
+			// To handle them all, we need to check the state again,
+			// so we go back to the start of the loop.
 		}
-		if value != lk.clientID {
-			// Error if it's locked to another client?
-			continue
-		}
-		err = lk.ck.Put(lk.lockID, UnlockedState, version)
-		if err != rpc.OK {
-			continue
-		}
-		return
 	}
 }
