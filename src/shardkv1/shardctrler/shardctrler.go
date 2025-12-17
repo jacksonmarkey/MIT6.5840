979c78ff6bae8a8980affa3b8a09f4d39bac25c4
Jackson Markey <jackson.markey@gmail.com>
2025-12-17 13:06:04 -0500

    implements InitConfig and Query methods for shardctrler initialization

--- src/shardkv1/shardctrler/shardctrler.go
+++ src/shardkv1/shardctrler/shardctrler.go
@@ -5,13 +5,14 @@ package shardctrler
 //
 
 import (
-
-	"6.5840/kvsrv1"
-	"6.5840/kvtest1"
+	kvsrv "6.5840/kvsrv1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 	"6.5840/shardkv1/shardcfg"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
+const CONFIG_KEY = "cfg_key"
 
 // ShardCtrler for the controller and kv clerk.
 type ShardCtrler struct {
@@ -45,6 +46,8 @@ func (sck *ShardCtrler) InitController() {
 // lists shardgrp shardcfg.Gid1 for all shards.
 func (sck *ShardCtrler) InitConfig(cfg *shardcfg.ShardConfig) {
 	// Your code here
+	s := cfg.String()
+	sck.IKVClerk.Put(CONFIG_KEY, s, 0)
 }
 
 // Called by the tester to ask the controller to change the
@@ -55,10 +58,13 @@ func (sck *ShardCtrler) ChangeConfigTo(new *shardcfg.ShardConfig) {
 	// Your code here.
 }
 
-
 // Return the current configuration
 func (sck *ShardCtrler) Query() *shardcfg.ShardConfig {
 	// Your code here.
-	return nil
+	s, _, err := sck.IKVClerk.Get(CONFIG_KEY)
+	for err != rpc.OK {
+		s, _, err = sck.IKVClerk.Get(CONFIG_KEY)
+	}
+	cfg := shardcfg.FromString(s)
+	return cfg
 }
-
